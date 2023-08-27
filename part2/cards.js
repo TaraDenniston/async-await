afa5eb1baf5bc9e4b60a5dfde5708c4de724f87a
Tara Denniston <taradenniston@gmail.com>
2023-08-26 20:02:17 -0400

    complete exercise 2.2

--- part2/cards.js
+++ part2/cards.js
@@ -14,3 +14,26 @@ async function drawOne() {
 }
 
 drawOne();
+
+
+// 2.
+async function drawTwo() {
+  let cards = [];
+
+  try {
+    let { data: c1 } = await axios.get(`${baseUrl}/new/draw`)
+    cards.push(c1.cards[0]);
+
+    let { data: c2 } = await axios.get(`${baseUrl}/new/draw`)
+    cards.push(c2.cards[0]);
+
+    cards.forEach(card => {
+      console.log(`${card.value} of ${card.suit}`)
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+drawTwo();
+
